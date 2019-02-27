528905407ed72fdcb4f04a8bf6e919176b8f23e0
Ayşenur Kara <aysenurk.95@outlook.com>
2019-02-27 14:55:09 +0300

    Angular js ile basit denemeler

---
+++ WepApiAngularJs/Scripts/app.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="angular-mocks.js" />
+
+var app = angular.module("myApp", []); 
+
+app.controller("ProductCtrl",
+	function ($scope) {
+		$scope.urunler = [];
+
+		$scope.ekle = function() {
+			$scope.urunler.push({
+				urunAdi: $scope.yeniKisi.urunAdi,
+				fiyat: $scope.yeni.fiyat,
+				eklemeZamani: new Date()
+			});
+		};
+	});
