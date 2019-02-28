312bbd2a5bee5316a96ad0188d4a2c1f4757ce91
Emre Çetin <cetinemre17@gmail.com>
2019-02-28 09:50:34 +0300

    ürün ara ürün sil

--- WepApiAngularJs/Scripts/app.js
+++ WepApiAngularJs/Scripts/app.js
@@ -1,16 +1,45 @@
-﻿/// <reference path="angular-mocks.js" />
+﻿/// <reference path="angular.js" />
 
-var app = angular.module("myApp", []); 
+var app = angular.module("myApp", []);
 
-app.controller("ProductCtrl",
-	function ($scope) {
-		$scope.urunler = [];
+app.controller("ProductCtrl", function ($scope) {
+	$scope.urunler = [];
 
-		$scope.ekle = function() {
-			$scope.urunler.push({
-				urunAdi: $scope.yeniKisi.urunAdi,
-				fiyat: $scope.yeni.fiyat,
-				eklemeZamani: new Date()
-			});
-		};
-	});
+	function init() {
+		var data = JSON.parse(localStorage.getItem("urunler"));
+		$scope.urunler = data === null ? [] : data;
+	}
+	$scope.ekle = function () {
+		$scope.urunler.push({
+			id: guid(),
+			urunAdi: $scope.yeni.urunAdi,
+			fiyat: $scope.yeni.fiyat,
+			eklenmeZamani: new Date()
+		});
+		$scope.yeni.urunAdi = "";
+		$scope.yeni.fiyat = "";
+		localStorage.setItem("urunler", JSON.stringify($scope.urunler));
+	};
+	$scope.sil = function(id) {
+		for (var i = 0; i < $scope.urunler.length; i++) {
+			var data = $scope.urunler[i];
+			if (id==data.id) {
+				$scope.urunler.splice(i, 1);
+				break;
+			}
+		}
+
+		localStorage.setItem("urunler", JSON.stringify($scope.urunler));
+	};
+
+	function guid() {
+		function S4() {
+			return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+		}
+
+		// then to call it, plus stitch in '4' in the third group
+		return (S4() + S4() + "-" + S4() + "-4" + S4().substr(0, 3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+	}
+
+	init();
+}); 
