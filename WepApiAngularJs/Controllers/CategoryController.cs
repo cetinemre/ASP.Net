a76b9bac6fa0fb36a21c429ccb8806f4ea24aaae
Emre Çetin <cetinemre17@gmail.com>
2019-02-28 15:00:19 +0300

    Web api giriş

---
+++ WepApiAngularJs/Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WepApiAngularJs.Controllers
+{
+    public class CategoryController : ApiController
+    {
+        public IHttpActionResult GetAll()
+        {
+            //try
+            //{
+            //    MyCon db = new MyCon();
+            //    return Ok(new
+            //    {
+            //        success=true,
+            //        data=db.Categories.Select(x=>
+                        
+            //            ToList()
+                   
+            //    })
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex);
+            //    throw;
+            //}
+        }
+    }
+}
