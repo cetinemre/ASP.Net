dfca02faedce88feabc0c3d7fe3b57eed6b06512
Emre Çetin <cetinemre17@gmail.com>
2019-03-05 14:51:38 +0300

    Odata

---
+++ DevexOdata/Controllers/WebApi/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using DevexOdata.Models;
+
+namespace DevexOdata.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        // GET: Customer
+        public IHttpActionResult GetAll()
+        {
+            ApplicationDbContext db= new ApplicationDbContext();
+            return Ok(new
+                {
+                    success =true,
+                    data=db.Customers.ToList()
+                });
+        }
+    }
+}
