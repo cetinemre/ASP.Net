2517b36186611b8f67cbb00be69254c36fc26ef4
Emre Çetin <cetinemre17@gmail.com>
2019-03-06 15:05:03 +0300

    Rabit mq

---
+++ Rabbit.BLL/Repository/MailLogRepo.cs
@@ -0,0 +1,13 @@
+﻿using Rabbit.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabbit.BLL.Repository
+{
+    public class MailLogRepo:RepositoryBase<MailLog,Guid>
+    {
+    }
+}
